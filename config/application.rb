49c2a10a535c4c84d7db2b673557e067c1485e68
Dushko Tasevski <dushkotasevski@webfactory.mk>
2019-01-16 18:29:02 +0100

    Fix RSPEC issues

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,19 @@ module MarketPlaceApi
     config.generators.system_tests = nil
   end
 end
+
+Rails.application.configure do
+  # don't generate RSpec tests for views and helpers
+  config.generators do |g|
+    g.test_framework :rspec, fixture: true
+    g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    g.view_specs false
+    g.helper_specs false
+    g.stylesheets = false
+    g.javascripts = false
+    g.helper = false
+  end
+
+  config.autoload_paths += %W(\#{config.root}/lib)
+end
+
