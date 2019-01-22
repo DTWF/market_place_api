4102f83526495ae4c40bf6efc4cb2607e61f5d22
Dushko Tasevski <dushkotasevski@webfactory.mk>
2019-01-22 15:31:13 +0100

    Edit user endpoints without constraints

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,5 @@ RSpec.describe User, type: :model do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
   it { should be_valid }
 end
