6bfaf42eed6988d21b9a92e09f0ef140d5ea8a69
Dushko Tasevski <dushkotasevski@webfactory.mk>
2019-01-25 15:46:37 +0100

    Adds the create method for the orders controller

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,9 +8,19 @@ RSpec.describe Order, type: :model do
   it { is_expected.to respond_to :total }
   it { is_expected.to respond_to :user_id }
   it { is_expected.to validate_presence_of :user_id }
-  it { is_expected.to validate_presence_of :total }
-  it { is_expected.to validate_numericality_of(:total).is_greater_than_or_equal_to(0) }
   it { is_expected.to belong_to :user }
   it { is_expected.to have_many :placements }
   it { is_expected.to have_many(:products).through(:placements) }
+
+  describe "#set_total!" do
+    before(:each) do
+      product_1 = FactoryBot.create :product, price: 100
+      product_2 = FactoryBot.create :product, price: 85
+      @order = FactoryBot.build :order, product_ids: [ product_1.id, product_2.id ]
+    end
+
+    it "returns the total amount for the products" do
+      expect{ @order.set_total! }.to change {@order.total}.from(0).to(185)
+    end
+  end
 end
