ac4e9785c12de8daab7ebc8680f424aab864f085
Dushko Tasevski <dushkotasevski@webfactory.mk>
2019-01-25 13:47:21 +0100

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::OrdersController < ApplicationController
-
+  respond_to :json
   def index
     respond_with current_user.orders
+  end
+
+  def show
+    respond_with current_user.orders.find(params[:id])
   end
 end
